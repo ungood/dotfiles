f26c9188f57d2afc447ee34f060abf68289efefd
Jason Walker <jwwalker@amazon.com>
2012-11-14 14:36:17 -0800

    amazon changes

--- zshrc/amazon/brazil.sh
+++ zshrc/amazon/brazil.sh
@@ -1,5 +1,6 @@
 export BRAZIL_WORKSPACE_DEFAULT_LAYOUT=short
 export BRAZIL_WORKSPACE_GITMODE=1
+export BRAZIL_WORKSPACE_CR_TARGET_GROUPS=goldfish-dev
 
 alias bb='brazil-build'
 alias bba='brazil-build apollo-pkg'
