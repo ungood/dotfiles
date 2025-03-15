abdbf8e5f2dfe83e276397ec50310e1546224530
Jason Walker <jason@onetrue.name>
2025-03-15 16:42:02 -0700

    Update .config/fish/functions/warn.fish

--- dot_config/fish/functions/warn.fish
+++ dot_config/fish/functions/warn.fish
@@ -1,3 +1,3 @@
-function warn --description "Emits a warning to stdoout"
-    echo (set_color $fish_color_error) $argv
+function warn --description "Emits a warning to stdout"
+    echo "[WARN]" (set_color $fish_color_error) $argv
 end
